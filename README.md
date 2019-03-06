3dcd0a868d8fc7f9658ec07bc9d2c767f76d4041
Daniel Williams <mail@daniel-williams.co.uk>
2019-03-06 13:28:52 +0000

    Mega update of stuff I should have been committing before now.

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ Daniel Williams
 
 This repository contains the text and code necessary to produce my thesis.
 
-The `chapters` directory contains the text of the thesis in `org-mode`
-format. These can be converted to numerous other formats using scripts
-in the `scripts/build` directory, with the makefile capable of
-producing these itself.
+* The `chapters` directory contains the text of the thesis in `org-mode` format. 
+  These can be converted to numerous other formats using scripts in the `scripts/build` directory, with the makefile capable of producing these itself.
+
+* The `figures` directory contains a number of the figures from the document, although many need to be generated (via the makefile in this directory).
+
+* The `notebooks` directory contains a number of Jupyter notebooks which can be used to generate the figures from this work.
+
+* The `scripts` directory contains scripts used to generate figures from this work, and a makefile which can generate a number of these scripts from jupyter notebooks in the `notebooks` directory.
+
+* The `bibliography` directory contains org files which describe the bibliography of the thesis, along with my notes on these works. `.bib` files can be generated via the main project makefile.
 
--- thesis/__init__.py
+++ thesis/__init__.py
@@ -3,9 +3,17 @@ Ths file contains a number of bits and pieces which are designed to make the scr
 """
 
 from matplotlib import rc, font_manager
+from math import atan2,degrees
+import numpy as np
+from matplotlib import rc, font_manager
+import matplotlib.patheffects as path_effects
 
 colors = {"blue": "#348ABD",
-          "red": "#E24A33"}
+          "red": "#E24A33",
+          "purple": "#988ED5",
+          "gray": "#777777",
+          "yellow": "#FBC15E",
+}
 
 figwidth = 6 # 2.5
 figheight = 6/1.616
@@ -51,3 +59,86 @@ def thesisify(f, height=1):
         
     f.tight_layout()
     return f
+
+
+def labelLine(line,x,label=None,align=True,**kwargs):
+    
+    ax = line.axes
+    xdata = line.get_xdata().value
+    ydata = line.get_ydata().value
+    
+    if (x < xdata[0]) or (x > xdata[-1]):
+        print('x label location is outside data range!')
+        return
+
+    #Find corresponding y co-ordinate and angle of the line
+    ip = 1
+    for i in range(len(xdata)):
+        if x < xdata[i]:
+            ip = i
+            break
+
+    y = ydata[ip-1] + (ydata[ip]-ydata[ip-1])*(x-xdata[ip-1])/(xdata[ip]-xdata[ip-1])
+
+    
+    
+    if not label:
+        label = line.get_label()
+
+    if align:
+        #Compute the slope
+        dx = xdata[ip] - xdata[ip-1]
+        dy = ydata[ip] - ydata[ip-1]
+        ang = degrees(atan2(dy,dx))
+
+        #Transform to screen co-ordinates
+        pt = np.array([x,y]).reshape((1,2))
+        trans_angle = ax.transData.transform_angles(np.array((ang,)),pt)[0]
+    else:
+        trans_angle = 0
+
+    y*=2
+        
+    #Set a bunch of keyword arguments
+    if 'color' not in kwargs:
+        kwargs['color'] = line.get_color()
+
+    if ('horizontalalignment' not in kwargs) and ('ha' not in kwargs):
+        kwargs['ha'] = 'center'
+
+    if ('verticalalignment' not in kwargs) and ('va' not in kwargs):
+        kwargs['va'] = 'center'
+
+    #if 'backgroundcolor' not in kwargs:
+    #    kwargs['backgroundcolor'] = ax.get_facecolor()
+
+    if 'clip_on' not in kwargs:
+        kwargs['clip_on'] = True
+
+    if 'zorder' not in kwargs:
+        kwargs['zorder'] = 2.5
+
+    label = ax.text(x,y,label,rotation=trans_angle,fontdict=lato, **kwargs)
+    label.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white'),
+                           path_effects.Normal()])
+
+def labelLines(lines,align=True,xvals=None,**kwargs):
+
+    ax = lines[0].axes
+    labLines = []
+    labels = []
+
+    #Take only the lines which have labels other than the default ones
+    for line in lines:
+        label = line.get_label()
+        if "_line" not in label:
+            labLines.append(line)
+            labels.append(label)
+
+    if xvals is None:
+        xvals = []
+        for line in lines:
+            xvals.append(line.get_data()[0][-1].value*0.9)
+
+    for line,x,label in zip(labLines,xvals,labels):
+        labelLine(line,x,label,align,**kwargs)
