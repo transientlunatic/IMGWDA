8e54a6528d46af6c9b02ad7dabad82a6629022a2
Daniel Williams <mail@daniel-williams.co.uk>
2019-04-02 13:52:44 +0100

    Made minor changes to the sed script to allow plurals.

--- scripts/build/glossary.py
+++ scripts/build/glossary.py
@@ -1,7 +1,28 @@
 from PyOrgMode import PyOrgMode
 import sys
 
+import string
+class PartialFormatter(string.Formatter):
+    def __init__(self, missing='', bad_fmt='!!'):
+        self.missing, self.bad_fmt=missing, bad_fmt
 
+    def get_field(self, field_name, args, kwargs):
+        # Handle a key not found
+        try:
+            val=super(PartialFormatter, self).get_field(field_name, args, kwargs)
+            # Python 3, 'super().get_field(field_name, args, kwargs)' works
+        except (KeyError, AttributeError):
+            val=None,field_name 
+        return val 
+
+    def format_field(self, value, spec):
+        # handle an invalid format
+        if value==None: return self.missing
+        try:
+            return super(PartialFormatter, self).format_field(value, spec)
+        except ValueError:
+            if self.bad_fmt is not None: return self.bad_fmt   
+            else: raise
 
 def parse_org_file(filename):
     """
@@ -22,19 +43,19 @@ def parse_org_file(filename):
         if hasattr(line, "heading"):
             entry['name'] = line.heading
             entry['description'] = ""
-            entry['pluralabb'] = ""
             for descline in line.content:
                 if hasattr(descline, "content"):
                     for orgproperty in descline.content:
                         if descline.TYPE == "DRAWER_ELEMENT":
                             try:
-                                entry[orgproperty.name] = orgproperty.value
+                                entry[orgproperty.name] = orgproperty.value#
+                                #.replace("{","[").replace("}","]")
                                 #print("+ {} : {}".format(orgproperty.name, orgproperty.value))
                             except AttributeError:
                                 #print(orgproperty)
                                 pass
                 else:
-                    entry['description'] += descline.replace("\n", " ")
+                    entry['description'] += descline#.replace("\n", " ")
         entries.append(entry)
     return entries
 
@@ -42,14 +63,26 @@ def export_latex(entries):
     """
     Produce a LaTeX-formatted glossary, using `glossaries`.
     """
-
+    fmt=PartialFormatter()
+    gloss_entry = """
+\\newglossaryentry{{{name}}}
+{{
+name={{{e[name]}}},
+symbol={{{e[SYMBOL]}}},
+descriptionplural={{{e[PLURAL]}}},
+description={{{e[description]}}}
+}}
+"""
+    
     for entry in entries:
         if "ABBREVIATION" in entry:
             print("\\newacronym{{{}}}{{{}}}{{{}}}".format(entry['ABBREVIATION'].lower(), entry['ABBREVIATION'], entry['name']))
 
     for entry in entries:
         if ("name" in entry) and ("description" in entry):
-            print("\\newglossaryentry{{{}}} {{ name={{{}}}, description={{{}}}, plural={{{}}} }}".format(entry['name'].lower().replace(" ", "-"), entry['name'], entry['description'], entry['pluralabb']))
+            output = fmt.format(gloss_entry, name=entry['name'].lower().replace(" ", "-"), e=entry)
+            output.replace("\\n", " ")
+            print(output)
 
 
 def main():
